3dcabe94917a08e2d9a94dcab44d755c2ff3a38f
Maximilian Flack <maximilian.flack@outlook.de>
2022-10-20 18:51:35 +0200

    VL3

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -4,11 +4,58 @@ namespace Template {
 
   console.log("FudgeStory template starting");
 
+  export let transition = {
+    puzzle: {
+      duration: 1,
+      alpha: "",
+      edge: 1
+    }
+  };
+
+  export let sound = {
+    // Themes
+
+    // SFX
+    coin: "Audio/coin.mp3"
+  };
+
+  export let locations = {
+    beachDay: {
+      name: "Beach Day",
+      background: ""
+    }
+  };
+
+  export let characters = {
+    narrator: {
+      name: "Thorsten"
+    },
+    protagonist: {
+      name: "Seppel"
+    },
+    aisaka: {
+      name: "Aisaka",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "PFAD ZUM PNG",
+        haappy: "PFAD ZUM PNG",
+        upset: "PFAD ZUM PNG"
+      }
+    }
+  };
+
+  export let dataForSave = {
+    nameProtagonist: ""
+  };
+
   window.addEventListener("load", start);
   function start(_event: Event): void {
     let scenes: ƒS.Scenes = [
       { scene: Scene, name: "Scene" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]");
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement);
 
     // start the sequence
     ƒS.Progress.go(scenes);
